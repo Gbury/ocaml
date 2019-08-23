Gbury/ocaml

commit 12273f27ce820c814a18b2c26f6325f443c37935

fixes post rebase

middle_end/flambda2.0/to_cmm/un_cps.ml
@@ -424,10 +424,8 @@ let unary_primitive env dbg f arg =
       C.extcall ~alloc:true "caml_obj_dup" typ_val [arg]
   | Is_int ->
       C.and_ ~dbg arg (C.int ~dbg 1)
-  | Get_tag _ ->
+  | Get_tag ->
       C.get_tag arg dbg
-  | Discriminant_of_int ->
-      C.untag_int arg dbg
   | Array_length block_access_kind ->
       C.block_length ~dbg block_access_kind arg
   | Bigarray_length { dimension } ->

commit 146e839ca263ab49a77348275696146820b784d8

Fix a bug introduced in 12273f27ce820c814a18b2c26f6325f443c37935
When Discriminant_of_int was removed, the still necessary untag_int
operation on the scrutinee simply disapeared, leading to uncorrect code
being generated. This restores the correct untag_int invocation by using
the switch Sort to determine whether it is needed.

middle_end/flambda2.0/to_cmm/un_cps.ml
@@ -863,8 +863,18 @@ and apply_cont env e =
         expr env body
   end
 
+and switch_scrutinee env sort s =
+  let e, env = simple env s in
+  let e =
+    match (sort : Switch.Sort.t) with
+    | Int -> C.untag_int e Debuginfo.none
+    | Tag _ -> e (* get_tag already returns untagged integers *)
+    | Is_int -> e (* Is_int already returns untagged integers *)
+  in
+  e, env
+
 and switch env s =
-  let e, env = simple env (Switch.scrutinee s) in
+  let e, env = switch_scrutinee env (Switch.sort s) (Switch.scrutinee s) in
   let wrap, env = Env.flush_delayed_lets env in
   let ints, exprs =
     Discriminant.Map.fold (fun d k (ints, exprs) ->